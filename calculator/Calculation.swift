152df4fca27831c6c7f6a2dc7be913fd26d03c3d
Sai Yashwant <hd7.yashwant@gmail.com>
2019-07-12 15:26:43 +0530

    revert delegation and connect buttons to ViewController

--- calculator/Calculation.swift
+++ calculator/Calculation.swift
@@ -9,120 +9,21 @@
 import Foundation
 
 protocol Calculation {
-    var previousExpressions: [(expression: String, result: String)] { get set }
-    var currentOperator: Operator? { get set }
     
-    var leftNumber: Double? { get set }
-    var rightNumber: Double? { get set }
-    var resultNumber: Double { get }
-    
-    var expressionString: String { get }
-    
-    func handleInput(_ number: Int)
-    func setOperator(_ newOperator: Operator)
-    func clearInputAndSave(_ save: Bool)
 }
 
 protocol Operator {
-    var character: String { get }
-    var operate: (Double, Double) -> Double { get }
-    init (forCharacter character: String, withFunction: @escaping (Double, Double) -> Double)
+    
 }
 
 class DefaultCalculationDelegate : Calculation {
-    
-    var previousExpressions: [(expression: String, result: String)] = []
-    var currentOperator: Operator?
-    
-    var leftNumber: Double?
-    var rightNumber: Double?
-    var resultNumber: Double {
-        if let leftNumber = leftNumber {
-            return rightNumber ?? leftNumber
-        }
-        return leftNumber ?? Double(previousExpressions.last?.result ?? "a") ?? 0.0
-    }
-    
-    var expressionString: String {
-        if let left = leftNumber?.roundedString {
-            if let operatorChar = currentOperator?.character {
-                let right = rightNumber?.roundedString ?? "0"
-                return "\(left) \(operatorChar) \(right) = "
-            }
-            return "\(left) = "
-        }
-        return "0 = "
-    }
-    
-    func handleInput(_ input: Int) {
-        
-        if expressionString == "" {
-            leftNumber = nil
-        }
-        
-        let useLeft = (currentOperator == nil)
-        
-        let optionalNumber = useLeft ? leftNumber : rightNumber
-        var newNumber = optionalNumber ?? 0.0
-        newNumber = (newNumber * 10) + Double(input)
-        
-        if useLeft { leftNumber = newNumber }
-        else { rightNumber = newNumber }
-    }
-    
-    func setOperator(_ newOperator: Operator) {
-        if leftNumber == nil {
-            leftNumber = Double(previousExpressions.last?.result ?? "a") ?? 0.0
-        }
-        
-        if rightNumber != nil {
-            clearInputAndSave(true)
-            leftNumber = resultNumber
-        }
-        
-        currentOperator = newOperator
-    }
-    
-    func clearInputAndSave(_ save: Bool) {
-        
-        if !save {
-            previousExpressions.append((expression: "cleared", result: ""))
-        }
-        
-        let result = currentOperator?.operate(leftNumber ?? 0.0, rightNumber ?? 0.0)
-        
-        if leftNumber != nil || rightNumber != nil || currentOperator != nil {
-            if let result = result {
-                previousExpressions.append((expression: expressionString, result: "\(result)"))
-            }
-        }
-        
-        leftNumber = nil
-        rightNumber = nil
-        currentOperator = nil
-    }
     
 }
 
 struct DefaultOperator : Operator {
     
-    var character: String
-    var operate: (Double, Double) -> Double
-    
-    init(forCharacter character: String, withFunction operate: @escaping (Double, Double) -> Double) {
-        self.character = character
-        self.operate = operate
-    }
-    
 }
 
-
 extension Double {
-    
-    var roundedString: String {
-        let rounded = (self * 100).rounded() / 100
-        let string = "\(rounded)"
-        return string.hasSuffix(".0") ? "\(Int(self))" : string
-    }
     
 }
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,21 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var resultVariable: UILabel!
+    let calculations: Calculation = DefaultCalculationDelegate()
+    
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func addButtonPressed(_ sender: UIButton) {
+        setOperator("+", withFunction: +)
     }
     @IBAction func subtractButtonPressed(_ sender: UIButton) {
+        setOperator("-", withFunction: -)
     }
     @IBAction func multiplyButtonPressed(_ sender: UIButton) {
+        setOperator("*", withFunction: *)
     }
     @IBAction func divideButtonPressed(_ sender: UIButton) {
+        setOperator("/", withFunction: /)
     }
     @IBAction func equalsButtonPressed(_ sender: UIButton) {
+        calculations.clearInputAndSave(true)
+        resultLabel.text = calculations.resultNumber.roundedString
     }
     @IBAction func clearButtonPressed(_ sender: UIButton) {
+        calculations.clearInputAndSave(false)
+        resultLabel.text = calculations.resultNumber.roundedString
     }
     
     @IBAction func numberButtonPressed(_ sender: UIButton) {
+        let buttonTitle = sender.titleLabel!.text!
+        let buttonNumber = Int(buttonTitle)!
+        calculations.handleInput(buttonNumber)
+        
+        resultLabel.text = calculations.resultNumber.roundedString
+    }
+    
+    func setOperator(_ character: String, withFunction function: @escaping (Double, Double) -> (Double)) {
+        let customOperator = DefaultOperator(forCharacter: character, withFunction: function)
+        calculations.setOperator(customOperator)
+        
+        resultLabel.text = calculations.resultNumber.roundedString
     }
 }
