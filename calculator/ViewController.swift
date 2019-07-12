9d97340d8449945b9c0683229c01d5c0a943b7f2
Sai Yashwant <hd7.yashwant@gmail.com>
2019-07-12 18:03:16 +0530

    clean and fix stuff

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -2,51 +2,18 @@
 //  ViewController.swift
 //  calculator
 //
-//  Created by Sai Yashwant on 11/07/19.
+//  Created by Venigalla, Sai on 12/07/19.
 //  Copyright © 2019 Random Inc. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let calculations: Calculation = DefaultCalculationDelegate()
-    
+
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func addButtonPressed(_ sender: UIButton) {
-        setOperator("+", withFunction: +)
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    @IBAction func subtractButtonPressed(_ sender: UIButton) {
-        setOperator("-", withFunction: -)
-    }
-    @IBAction func multiplyButtonPressed(_ sender: UIButton) {
-        setOperator("*", withFunction: *)
-    }
-    @IBAction func divideButtonPressed(_ sender: UIButton) {
-        setOperator("/", withFunction: /)
-    }
-    @IBAction func equalsButtonPressed(_ sender: UIButton) {
-        calculations.clearInputAndSave(true)
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    @IBAction func clearButtonPressed(_ sender: UIButton) {
-        calculations.clearInputAndSave(false)
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    
-    @IBAction func numberButtonPressed(_ sender: UIButton) {
-        let buttonTitle = sender.titleLabel!.text!
-        let buttonNumber = Int(buttonTitle)!
-        calculations.handleInput(buttonNumber)
-        
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    
-    func setOperator(_ character: String, withFunction function: @escaping (Double, Double) -> (Double)) {
-        let customOperator = DefaultOperator(forCharacter: character, withFunction: function)
-        calculations.setOperator(customOperator)
-        
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
+
 }
